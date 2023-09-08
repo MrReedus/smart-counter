d97bf17f24af18b24b23f563ed7a0b8688bfd375
Evgenii <naterid@yandex.ru>
2023-09-08 12:22:41 +0700

    add styles for setting block

--- src/components/SettingBlock/index.tsx
+++ src/components/SettingBlock/index.tsx
@@ -6,9 +6,11 @@ const SettingBlock = ({}) => {
     return (
 
         <div className="counter">
-            <div className="setting-inputs">
-                <label >max value<input type="number"/></label>
-                <label >start value<input type="number"/></label>
+            <div className="counter__value">
+                <div className="setting-inputs">
+                    <label>max value<input type="number"/></label>
+                    <label>start value<input type="number"/></label>
+                </div>
             </div>
 
             <div className="counter__buttons">
